222e8cbd56169ed1c705f8c99e197b360aa07c6a
Eugene Zubkov <evgv@users.noreply.github.com>
2017-08-15 16:52:33 +0300

    Update Helloworld.php

--- Block/Helloworld.php
+++ Block/Helloworld.php
@@ -1,1 +1,14 @@
 
+<?php
+
+namespace Vndr\Helloworld\Block;
+
+use Magento\Framework\View\Element\Template;
+ 
+class Helloworld extends Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
